86df424794dba62e7d95f5f7427cf421adc7d242
Adam Callanan <a.callanan@sbcglobal.net>
2020-03-13 22:48:57 -0500

    clean up admin panel

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -32,26 +32,6 @@ export class AdminComponent implements OnInit {
     this.navHelper.goToAddUser();
   }
 
-  public toggleAdmin(user: User) {
-    let dialogText;
-    if (user.admin) {
-      dialogText = `Are you sure you want to remove the admin rights for ${user.email}?`;
-    } else {
-      dialogText = `Are you sure you want to give admin rights to ${user.email}?`;
-    }
-    const confirmToggle = confirm(dialogText);
-    if (confirmToggle) {
-      this.submitToggleAdmin(user);
-    }
-  }
-
-  public setSpecialAccess(user: User) {
-    const newAccess = prompt(`Access is currently: ${user.specialAccess}. What would you like to change it to?`);
-    if (newAccess) {
-      this.editAccess(user, newAccess);
-    }
-  }
-
   public deleteUser(user: User) {
     const confirmDelete = confirm(`Are you sure you want to delete ${user.email}?`);
     if (confirmDelete) {
@@ -63,18 +43,6 @@ export class AdminComponent implements OnInit {
     this.navHelper.goToEditUser(user._id);
   }
 
-  private editAccess(user: User, newAccess: string) {
-    let response;
-    this.userService.editAccess(user.email, newAccess)
-      .subscribe((res) => response = res,
-        (error) => {
-          console.log(error);
-          console.log("edit access failed");
-        }, () => {
-          this.loadUsers();
-        });
-  }
-
   private submitDelete(user: User) {
     let response;
     this.userService.deleteUser(user._id)
@@ -82,18 +50,6 @@ export class AdminComponent implements OnInit {
         (error) => {
           console.log(error);
           console.log("delete user failed");
-        }, () => {
-          this.loadUsers();
-        });
-  }
-
-  private submitToggleAdmin(user: User) {
-    let response;
-    this.userService.setAdmin(user.email, !user.admin)
-      .subscribe((res) => response = res,
-        (error) => {
-          console.log(error);
-          console.log("edit admin failed");
         }, () => {
           this.loadUsers();
         });
