7e90cbc1c62ccdeca4b292f84884fd29080d8af8
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-07 20:56:39 -0500

    add context to dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { CookieHelper } from 'src/app/utilities/cookie.util';
+import { NavHelperService } from 'src/app/services/nav-helper.service';
 
 @Component({
   selector: "app-dashboard",
@@ -6,4 +8,36 @@ import { Component } from "@angular/core";
   styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent {
+  public get isLoggedOn(): boolean {
+    return CookieHelper.isLoggedOn;
+  }
+
+  public get admin(): boolean {
+    return CookieHelper.admin;
+  }
+
+  constructor(
+    private navHelper: NavHelperService,
+  ) { }
+
+  public goToProfile() {
+    this.navHelper.goToProfile();
+  }
+
+  public goToAdmin() {
+    this.navHelper.goToAdmin();
+  }
+
+  public goToLogin() {
+    this.navHelper.goToLogin();
+  }
+
+  public goToRegister() {
+    this.navHelper.goToRegister();
+  }
+
+  public logout() {
+    CookieHelper.removeToken();
+    this.navHelper.goToLogin();
+  }
 }
